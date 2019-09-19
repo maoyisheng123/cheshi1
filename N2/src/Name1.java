1e00f59af7a7dbdffcc785e08bc463b709dad3a4
a206 <a206@10.20.63.180>
2019-09-19 10:23:30 +0800

    修改代码

--- N2/src/Name1.java
+++ N2/src/Name1.java
@@ -4,6 +4,7 @@ public class Name1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello world");
+System.out.println("Hello GitHub");
 	}
 
 }
